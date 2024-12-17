a135b5831e1e7c141d7ce5f0897a285ee7c9a698
Hernan <hernan.olmedo@usach.cl>
2024-12-17 17:13:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Desafío Chile Pasajes',
+  webDir: 'www'
+};
+
+export default config;
